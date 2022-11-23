d37da38301287ef2089f307298fe785cdf354a1b
Abhishek Shrestha <absk.shrestha@gmail.com>
2022-11-23 15:12:18 +0545

    resgister page UI

--- lib/auth/login_page.dart
+++ lib/auth/login_page.dart
@@ -19,9 +19,9 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).primaryColor,
-        ),
+        // appBar: AppBar(
+        //   backgroundColor: Theme.of(context).primaryColor,
+        // ),
         body: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
--- lib/auth/register_page.dart
+++ lib/auth/register_page.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/widgets.dart';
+import '../auth/login_page.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -8,11 +12,158 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final formKey = GlobalKey<FormState>();
+  String email = '';
+  String password = '';
+  String fullName = '';
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Register Page'),
+    return SafeArea(
+      child: Scaffold(
+        // appBar: AppBar(
+        //   backgroundColor: Theme.of(context).primaryColor,
+        // ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Text Me',
+                      style:
+                          TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Create your account to text me',
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+                    ),
+                    Image.asset("assets/register.png"),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      decoration: textInputDecoration.copyWith(
+                        labelText: 'Full Name',
+                        hintText: 'Enter your full name',
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      validator: (val) {
+                        if (val!.isNotEmpty) {
+                          return null;
+                        } else {
+                          return 'Please enter your full name';
+                        }
+                      },
+                      onChanged: (val) {
+                        setState(() {
+                          fullName = val;
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      decoration: textInputDecoration.copyWith(
+                        labelText: 'Email',
+                        hintText: 'Enter your Email ID',
+                        prefixIcon: Icon(
+                          Icons.email,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      onChanged: (val) {
+                        setState(() {
+                          email = val;
+                        });
+                      },
+                      validator: (val) {
+                        return RegExp(
+                                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                .hasMatch(val!)
+                            ? null
+                            : "Please provide a valid email";
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: textInputDecoration.copyWith(
+                        labelText: 'Password',
+                        hintText: 'Enter your password',
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      validator: (val) {
+                        if (val!.length < 6) {
+                          return 'Password must be at least 6 characters';
+                        } else {
+                          return null;
+                        }
+                      },
+                      onChanged: (val) {
+                        setState(() {
+                          password = val;
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(context).primaryColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          elevation: 0,
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          'Register',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text.rich(
+                      TextSpan(
+                        text: 'Already have an account? ',
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Login now',
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              decoration: TextDecoration.underline,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                nextScreen(context, const LoginPage());
+                                // navigate to desired screen
+                              },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]),
+            ),
+          ),
+        ),
       ),
     );
   }
