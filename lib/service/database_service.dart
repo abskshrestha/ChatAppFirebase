1ed91d3d264ed2c30465fa389272b3ecb9c71d06
Abhishek Shrestha <absk.shrestha@gmail.com>
2022-11-28 21:09:42 +0545

    register authentication

--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -1,12 +1,28 @@
-class DatabaseService {
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class DatabaseService {
   final String? uid; //paramater
   DatabaseService({
     this.uid, //constructer
   });
 
-  Future UpdateUserData(String email, String password) async {
+  //reference for the collections
+  final CollectionReference userCollection =
+      FirebaseFirestore.instance.collection('users');
 
+  final CollectionReference groupCollection =
+      FirebaseFirestore.instance.collection('grpups');
+
+  //updating the collection
+  Future UpdateUserData(String fullName, String email) async {
+    return await userCollection.doc(uid).set({
+      'fullName': fullName,
+      'email': email,
+      'groups': [],
+      'profilPic': "",
+      'uid': uid,
+    });
   }
-  
 }
+
+//medium article change flutter database rules in firestore uid = uid
