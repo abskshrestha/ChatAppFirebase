eeb96ba99e24753ff2c5b2e42cd0575b5bddc3e4
Abhishek Shrestha <absk.shrestha@gmail.com>
2022-12-05 17:41:38 +0545

    added drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:chatapp_firebase/service/auth_service.dart';
 import 'package:flutter/material.dart';
+import '../helper/helper_function.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,16 +10,133 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String email = "";
+  String userName = "";
+
+  @override
+  void initState() {
+    gettingUserData();
+    super.initState();
+  }
+
+  gettingUserData() async {
+    await HelperFunctions.getUserEmailFromSF().then((value) {
+      setState(() {
+        email = value!;
+      });
+    });
+
+    await HelperFunctions.getUserNameFromSF().then((val) {
+      setState(() {
+        userName = val!;
+      });
+    });
+  }
+
   AuthService authService = AuthService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Text Me',
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 27),
+        ),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).primaryColor,
+        elevation: 0,
+      ),
       body: Center(
         child: ElevatedButton(
           child: Text('Logout'),
           onPressed: () {
             authService.signOut();
           },
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const Icon(
+              Icons.account_circle,
+              size: 150,
+              color: Colors.black,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              userName,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            const Divider(
+              height: 2,
+              thickness: 1,
+            ),
+            ListTile(
+              title: const Text(
+                'Groups',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
+              selectedColor: Theme.of(context).primaryColor,
+              selected: true,
+              leading: const Icon(
+                Icons.group,
+                color: Colors.black,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text(
+                'Settings',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
+     
+              leading: const Icon(
+                Icons.settings,
+                color: Colors.black,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text(
+                'Logout',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
+       
+              leading: const Icon(
+                Icons.exit_to_app,
+                color: Colors.black,
+              ),
+              onTap: () {},
+            ),
+
+          ],
         ),
       ),
     );
